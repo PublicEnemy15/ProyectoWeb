454353d1d49d17392e4e81e68aaf89b142be59d1
Polux <poluxmorales1@gmail.com>
2025-07-10 10:25:13 -0500

    Quitando prueba api

--- api/BDC.py
+++
@@ -1,7 +0,0 @@
-import mysql.connector
-conn = mysql.connector.connect(user = 'root', 
-                                     password = 'root',
-                                     host = 'localhost',
-                                     database = 'ProyectoTallerWeb',
-                                     port='3306')
-cursor = conn.cursor()
--- api/prueba.js
+++
@@ -1,8 +0,0 @@
-const url = "http://127.0.0.1:8000/users"
-const options = {
-    method: 'GET',
-    headers: {
-        'accept': 'application/json'
-    }
-}
-fetch(url, options).then(response => response.json()).then(data => {console.log(data)}).catch(error => console.error("Error:", error));
