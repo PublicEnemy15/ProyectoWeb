1b188058b826c9b0c8d2e39682e961cb8cb2173c
Franco <bounszz@hotmail.com>
2025-07-15 15:17:45 -0500

    arreglando about

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-
 import vercel from '@astrojs/vercel';
 
-// https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-
-  adapter: vercel()
+  
+  adapter: vercel(),
+  
+  build: {
+    inlineStylesheets: 'auto',
+  },
+  
+  prefetch: true, 
+  
+  compressHTML: true, 
 });
